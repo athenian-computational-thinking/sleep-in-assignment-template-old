c5f56a10b6215f11ad911ffef0ec7086f729b78d
Megan Leich <mleich@athenian.org>
2020-04-15 02:16:19 +0000

    added more instructions

--- my_code.py
+++ my_code.py
@@ -1,6 +1,8 @@
 def sleep_in(weekday, vacation):
    # Insert code here
+   # Use conditionals to check if weekday is True and/or if vacation is true. 
+   #Use "return True" if you sleep in and "return False" if you don't.
 
 if __name__ == '__main__':
-    # Test your code here. Change the argument to try different values
+    # Test your code here. Change the arguments (the parts in the parentheses) to try different values
     print(sleep_in(True, True))
